0aa16ad95667d6da748e08282190198054666f9f
Yousssefch <youssefchakir003@gmail.com>
2023-02-02 16:32:13 -0500

    programmed the basic timer code

--- study-web/src/Timer.js
+++ study-web/src/Timer.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React ,{useState,useEffect} from "react";
 import { ReactDOM } from "react";
 import './style.css';
 
-var minutes = Math.floor((20 % (1000 * 60 * 60)) / (1000 * 60));
-document.getElementById()
+
 function Timer() {
+    let startingMinutes = 20;
+    let startingSeconds = 0;
+    const [countDownSecond,setCountSecond] = useState(startingSeconds);
+    const [countDownMinute,setCountMinute] = useState(startingMinutes);
+    useEffect(()=>{
+        let interval = setInterval(()=>{
+            updateTime();
+
+        },1000);
+        return () => clearInterval(interval);
+    })
+    function updateTime() {
+        if(countDownSecond>0){
+            setCountSecond(countDownSecond-1);
+        }
+        if (countDownSecond===0) {
+            if (countDownMinute>0) {
+                setCountSecond(59);
+                setCountMinute(countDownMinute-1);
+            }
+        }
+        console.log("minutes:"+countDownMinute+"seondes:"+countDownSecond);
+    }
     return(
         <div className="timer">
         <h1 className="text-timer" id="Countdown">20:00</h1>
